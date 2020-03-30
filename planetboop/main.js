9f41713e3e807da4182ac7e034183c6d2ae224ac
Andrew <andcachia@gmail.com>
2020-03-31 00:31:27 +0200

    Website - Fixed screen responsiveness + animations

--- planetboop/main.js
+++ planetboop/main.js
@@ -14,7 +14,7 @@ new Vue({
             { id:1, title: 'Face Zapper', url_param: 'zapper', thumbnail_source: "Assets/Zapper Thumbnail.png", gameplay_img_src: "Assets/Zapper Thumbnail.png" },
             { id:2, title: 'zBoop', url_param: 'zboop', thumbnail_source: "Assets/PlanetBoop - Coming Soon.jpeg", gameplay_img_src: "Assets/PlanetBoop - Coming Soon.jpeg" },
             { id:3, title: 'Airship Release',  url_param: 'airship', thumbnail_source: "Assets/Airship Thumbnail.png", gameplay_img_src: "Assets/Airship Thumbnail.png" },
-            { id:4, title: 'Memory Game',  url_param: 'memorygame', thumbnail_source: "Assets/Memory Game Thumbnail2.png", gameplay_img_src: "Assets/Memory Game Thumbnail2.png" },
+            { id:4, title: 'Memory Game',  url_param: 'memory_game', thumbnail_source: "Assets/Memory Game Thumbnail2.png", gameplay_img_src: "Assets/Memory Game Thumbnail2.png" },
             { id:5, title: 'Stack-A-Boop',  url_param: 'stack_a_boop', thumbnail_source: "Assets/PlanetBoop - Coming Soon.jpeg", gameplay_img_src: "Assets/PlanetBoop - Coming Soon.jpeg" }
         ],
         selected_game: { },
@@ -31,11 +31,13 @@ new Vue({
 
             if (!gameViewer.classList.contains('closed') && this.selected_game == game){
                 gameViewer.classList.add('closed');
+                gameViewer.classList.remove('opened');
             }
             else{
                 this.selected_game = game;
                 if (gameViewer.classList.contains('closed')) {
                     gameViewer.classList.remove('closed');
+                    gameViewer.classList.add('opened');
                 }
             }
         },
@@ -48,40 +50,40 @@ new Vue({
 })
 
 $(document).ready(function(){
-$('.games-carousel-new').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-        {
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-        }
-        },
-        {
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }
-        },
-        {
-        breakpoint: 480,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
+    $('.games-carousel-new').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
 
