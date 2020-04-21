f4c1e2eb030b6859408b772878218ddaceaac68c
Andrew <andcachia@gmail.com>
2020-04-21 10:12:43 +0200

    Updated phaser + splashscreens + min js files

--- planetboop/play.js
+++ planetboop/play.js
@@ -1,10 +1,28 @@
-$(function() {
+(function() {
     var search = window.location.search;
     var game = search.replace("?game=", "");
 
-    var srcString = $("#inlineFrame").attr("src");
+    var iFrame = document.getElementById("inlineFrame");
+
+    if (game == "memory_game"){
+        document.getElementById("phone").toggle('portrait');
+        document.getElementById("phone").toggle('landscape');
+        iFrame.toggle('portrait');
+        iFrame.toggle('landscape');
+    }
+
+    var srcString = iFrame.getAttribute("src");
     var updatedSrc = srcString.replace("{0}", game);
     
-    $("#inlineFrame").attr("src", updatedSrc);
-});
+    iFrame.setAttribute("src", updatedSrc);
+})();
 
+new Vue({
+    el: '#editor',
+    computed: {
+    },
+    methods: {
+    },
+    created: function() {
+    }
+})
